bd9325ad92f19efba2fec1034acd36da9e811d16
Mukesh Kumar <singh88rajat@gmail.com>
2017-01-02 23:38:53 +0530

    Problem Statement

---
+++ src/test.java
@@ -0,0 +1,16 @@
+
+public class test {
+	static{
+		 
+		 print(10);
+	 }
+	static void print(int x){
+		
+		System.out.println(x); 
+		System.exit(0);
+	}
+
+}
+/* 
+Its showing "Selection doesn't contain a main type" error while running the program.
+*/
